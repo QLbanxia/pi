53a5794dd6f75276a03d32d9c6c3e280ca0f7880
QLbanxia <52033151+QLbanxia@users.noreply.github.com>
2020-09-24 15:05:48 +0800

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+
+wget https://download.visualstudio.microsoft.com/download/pr/e6456209-63c8-43fc-ba2d-11c43c9eacd5/3a12e6bae9ff57c1964eb83cb01604b6/dotnet-sdk-5.0.100-rc.1.20452.10-linux-arm.tar.gz
+mkdir -p /opt/dotnet && tar zxf dotnet-sdk-5.0.100-rc.1.20452.10-linux-arm.tar.gz -C /opt/dotnet
+echo 'DOTNET_ROOT=/opt/dotnet'>>/home/pi/.profile
+echo 'PATH=$PATH:/opt/dotnet'>>/home/pi/.profile
+
 
